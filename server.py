e5a6c595321641a0d50327f211d3170843ea3025
v.ivashchenko <v.ivashchenko@mac146rds.local>
2018-12-16 20:32:21 +0300

    add cors enable

--- server.py
+++ server.py
@@ -4,13 +4,18 @@ from flask import jsonify
 from flask import request
 from flask import Response
 from magic.magic import MagicWorker
+from flask_cors import CORS, cross_origin
+
 #import main
 #from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 answer =""
+cors = CORS(app)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/data') # take note of this decorator syntax, it's a common pattern
+@cross_origin()
 def hello():
     jsonResp =  { "dialogue":["Welcome to our bank", "Hi can you help me with credit cards", "Please, waiting", "Theme: credit cards", "Routing to expert", "Its not relevant sorry bye"], "emotions":  [{"joy":"70%"},{"annoyance":"40%"}]} # test_check_data
     print(jsonify(jsonResp))
@@ -18,6 +23,7 @@ def hello():
 
 
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+@cross_origin()
 def download(filename):
     uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
     return send_from_directory(directory=uploads, filename=filename)
@@ -27,6 +33,7 @@ def get_hello():
     return random.choice(greeting_list)
 
 @app.route('/postjson', methods = ['POST'])
+@cross_origin()
 def postJsonHandler():
     print (request.is_json)
     content = request.get_json()
